327e3edbb239b4f90ecb531a4ae51a82077b18e3
prettyletto <danielltw2@gmail.com>
2026-02-19 15:52:30 -0300

    store io

--- internal/infra/storage/config_io.go
+++ internal/infra/storage/config_io.go
@@ -1,4 +1,38 @@
 package storage
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
 
+	"github.com/Prettyletto/Allyas/internal/domain/models"
+)
 
+func LoadStorage(path string) (models.Store, error) {
+	var store models.Store
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return store, err
+	}
+
+	if len(data) == 0 {
+		return store, errors.New("config file is empty")
+	}
+
+	if err := json.Unmarshal(data, &store); err != nil {
+		return store, err
+	}
+	return store, nil
+}
+
+func SaveStorage(path string, store models.Store) error {
+	data, err := json.MarshalIndent(store, "", " ")
+	if err != nil {
+		return err
+	}
+	if _, err := EnsureAppConfigDir(); err != nil {
+		return nil
+	}
+
+}
